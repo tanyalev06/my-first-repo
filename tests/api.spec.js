b2d7089533f8a71cae8450073e7dac2e466d4e2c
Tatyana Levchenko <grib.tanya06@mail.ru>
2026-02-02 21:11:06 +0300

    feat: change  api.spec.js file

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -6,6 +6,7 @@ test.describe('API-тесты для Restful-broker', () => {
 
     const baseURL = 'https://restful-booker.herokuapp.com';
     let bookingID; //хранение bookingID, полученного после создания брони
+    let authToken;
     const bookingData = {
         "firstname" : "Jim",
         "lastname" : "Brown",
@@ -72,6 +73,61 @@ test.describe('API-тесты для Restful-broker', () => {
         expect(responseBody.bookingdates).toEqual(bookingData.bookingdates);
         expect(responseBody.additionalneeds).toBe(bookingData.additionalneeds);
     })
-
     
+    //UPDATE
+    test ('Обновление бронирования', async ({request}) => {
+        //Получение токена авторизации
+        const authResponse = await request.post(`${baseURL}/auth`, {
+            data: {
+                username: "admin",
+                password: "password123"
+            }
+        });
+        const {token} = await authResponse.json();
+        authToken = token;
+        console.log('Токен авторизации:', token);
+
+        //Данные для обновления
+        const updateBooking = {
+            ...bookingData,
+            totalprice: 222
+        }
+
+        //Отправление PUT-запроса на обновление брони
+        const response = await request.put(`${baseURL}/booking/${bookingID}`, {
+            data: updateBooking,
+            headers: {
+                Cookie: `token=${token}`,
+                Accept: 'application/json'
+            }
+        });
+        console.log(`Статус-код PUT: ${response.status()}`);
+        expect(response.status()).toBe(200);
+
+        // Проверка 1: Получаем тело ответа
+        const responseBody = await response.json();
+        console.log('Ответ PUT:', responseBody);
+
+        // Проверка 2: Данные бронирования обновлены
+        expect(responseBody.totalprice).toBe(updateBooking.totalprice);
+    });
+
+    test ('Удаление бронирования', async ({request}) => {
+        // Отправляем DELETE запрос с токеном
+        const response = await request.delete(`${baseURL}/booking/${bookingID}`, {
+             headers: { Cookie: `token=${authToken}` }
+        });
+
+        //Проверка 1: Статус-код ответа равен 201 Created
+        console.log(`Статус-код DELETE: ${response.status()}`);
+        expect(response.status()).toBe(201);
+
+        //Проверка2: Cервер отвечает со статус-кодом 404 Not Found
+        const getResponse = await request.get(`${baseURL}/booking/${bookingID}`, {
+            headers: {Accept: 'application/json'}
+        });
+
+        console.log(`Статус-код GET после DELETE: ${getResponse.status()}`);
+        expect(getResponse.status()).toBe(404);
+    })
 });
